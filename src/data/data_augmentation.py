43bc426b98f23818f44c41ec512a9af828087cba
ZPavlo <pzakala@gmail.com>
2020-04-28 04:07:21 +0300

    Add aygmentation

---
+++ src/data/data_augmentation.py
@@ -0,0 +1,82 @@
+from tqdm import tqdm
+import numpy as np
+import cv2 
+
+def rotation_changing(image):
+    # Defining angle range
+    angle_range = 25
+    # Defining angle rotation
+    angle_rotation = np.random.uniform(angle_range) - angle_range / 2
+    # Getting shape of image
+    rows, columns, channels = image.shape
+    # Implementing rotation
+    # Calculating Affine Matrix
+    affine_matrix = cv2.getRotationMatrix2D((columns / 2, rows / 2),
+                                            angle_rotation, 1)
+    # Warping original image with Affine Matrix
+    rotated_image = cv2.warpAffine(image, affine_matrix, (columns, rows))
+    # Returning rotated image
+    return rotated_image
+
+def brightness_changing(image):
+    # Converting firstly image from RGB to HSV
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    # Defining random value for changing brightness
+    random_brightness = 0.25 + np.random.uniform()
+    # Implementing changing of Value channel of HSV image
+    image_hsv[:, :, 2] = image_hsv[:, :, 2] * random_brightness
+    # Converting HSV changed image to RGB
+    image_rgb = cv2.cvtColor(image_hsv, cv2.COLOR_HSV2RGB)
+    # Returning image with changed brightness
+    return image_rgb
+
+# Defining function for transformation: brightness + rotation
+def transformation_brightness_rotation(image):
+    return brightness_changing(rotation_changing(image))
+
+
+# Defining function for getting random image of one label
+def random_image(x_train, y_train, y_number):
+    # Getting indexes of needed 'y_number' from 'y_train'
+    # Defining True - False array
+    image_indexes = np.where(y_train == y_number)
+    # Getting random index of needed label
+    # 'np.bincount(y_train)' - array with number of examples for every label
+    # 'np.bincount(y_train)[y_number] - 1' - number of examples for 'y_number' label
+    random_index = np.random.randint(0, np.bincount(y_train)[y_number] - 1)
+    # Returning random image from 'x_train'
+    # 'x_train[image_indexes]' - returns array with only 'y_number' label
+    # 'x_train[image_indexes][random_index]' - random image of needed label
+    return x_train[image_indexes][random_index]
+
+
+# Defining function for equalization training dataset
+def equalize_training_dataset(x_train, y_train):
+    # Getting number of examples for every label
+    number_of_examples_for_every_label = np.bincount(y_train)
+    # Calculating total amount of unique labels
+    number_of_labels = np.arange(len(number_of_examples_for_every_label))
+
+    # Iterating over all number of labels
+    # Showing progress ber with 'tqdm'
+    for i in tqdm(number_of_labels):
+        # Calculating how many examples is needed to add for current label
+        # 'np.mean(number_of_examples_for_every_label)' - average number over examples for every label
+        number_of_examples_to_add = int(
+            np.mean(number_of_examples_for_every_label) * 2.5) - \
+                                    number_of_examples_for_every_label[i]
+
+        # Defining temporary arrays for collecting new images
+        x_temp = []
+        y_temp = []
+
+        # Getting random image from current label
+        # Transforming it and adding to the temporary arrays
+        for j in range(number_of_examples_to_add):
+            getting_random_image = random_image(x_train, y_train, i)
+            x_temp.append(
+                transformation_brightness_rotation(getting_random_image))
+            y_temp.append(i)
+
+        x_train = np.append(x_train, np.array(x_temp), axis=0)
+        y_train = np.append(y_train, np.array(y_temp), axis=0)
